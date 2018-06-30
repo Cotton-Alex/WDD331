26b4707f926c063f96de1b8844688da56703c55d
Alex <cot15011@byui.edu>
2018-06-30 01:19:42 -0700

    week 10

---
+++ Week10-Hamburger/js.js
@@ -0,0 +1,8 @@
+function toggle() {
+    var updateElement = document.getElementById("burgerId");
+    updateElement.classList.toggle("open");
+    var updateNav = document.getElementById("nav");
+    updateNav.classList.toggle("open");
+    var updateSectionOne = document.getElementById("sectionOne");
+    updateSectionOne.classList.toggle("open");
+}   
